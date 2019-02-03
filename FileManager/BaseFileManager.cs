2d0c2bf3862e0d7994a664a14c08753fe057a4ad
Dimitri Pankov <dimon2255@gmail.com>
2019-02-03 12:07:24 -0500

    Initial Commit

---
+++ FileManager/BaseFileManager.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+using static Dna.DI;
+
+
+namespace Dna
+{
+    /// <summary>
+    /// Handles reading/writing and querying the file system
+    /// </summary>
+    public class BaseFileManager : IFileManager
+    {
+
+
+        /// <summary>
+        /// Writes all text to a file
+        /// </summary>
+        /// <param name="path">Path of the file</param>
+        /// <param name="text">Text to write to the file</param>
+        /// <param name="append">Flag indicating whether to append or overwrite a file</param>
+        /// <returns>Task for Continuation</returns>
+        public async Task WriteTextToFileAsync(string text, string path, bool append = false)
+        {
+            //TODO: Add exception catching
+
+            path = NormalizePath(path);
+            path = ResolvePath(path);
+
+
+            //path = path.Replace("/", "\\").Trim();
+
+            //Lock the task
+            await AsyncAwaiter.AwaitAsync(nameof(BaseFileManager) + path, async () =>
+             {
+                 //Run the synchronous file access as a new task
+                 await TaskManager.Run(() =>
+                 {
+                     //Write the log message to file
+                     using (var fileStream = (TextWriter)new StreamWriter(File.Open(path, append ? FileMode.Append : FileMode.Create)))
+                     {
+                         fileStream.Write(text);
+                     }
+                 });
+             });
+        }
+
+        /// <summary>
+        /// Normalizes a file path based on operating system
+        /// </summary>
+        /// <param name="path">file path</param>
+        /// <returns></returns>
+        public string NormalizePath(string path)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    return path?.Replace('/', '\\').Trim();            
+            else
+                return path?.Replace('\\', '/').Trim();
+        }
+
+
+        /// <summary>
+        /// Resolves a path to an absolute path
+        /// </summary>
+        /// <param name="path">file path</param>
+        /// <returns></returns>
+        public string ResolvePath(string path)
+        {
+            return Path.GetFullPath(path);
+        }
+    }
+}
---
+++ FileManager/IFileManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace Dna
+{
+    /// <summary>
+    /// Handles reading/writing and querying the file system
+    /// </summary>
+    public interface IFileManager
+    {
+        /// <summary>
+        /// Writes all text to a file
+        /// </summary>
+        /// <param name="path">Path of the file</param>
+        /// <param name="text">Text to write to the file</param>
+        /// <param name="append">Flag indicating whether to append or overwrite a file</param>
+        /// <returns>Task for Continuation</returns>
+        Task WriteTextToFileAsync(string text, string path, bool append = false);
+
+        /// <summary>
+        /// Normalizes a file path based on the current operating system
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        string NormalizePath(string path);
+
+        /// <summary>
+        /// Resolves a path to an absolute path
+        /// </summary>
+        /// <param name="path">file path</param>
+        /// <returns></returns>
+        string ResolvePath(string path);
+    }
+}
